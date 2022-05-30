9380ae616e4486ad2ed53411a3a271dd388561a5
hafiz-aa <mhafiz.aa@gmail.com>
2022-05-30 14:46:51 +0700

    commit on master

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,26 +1,21 @@
 <template>
   <div class="post-list">
-    <div v-for="post in posts" :key="post.id" >
+    <div v-for="post in posts" :key="post.id">
       <SinglePost :post="post" />
     </div>
   </div>
 </template>
 
 <script>
-import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core'
-import SinglePost from '../components/SinglePost.vue'
-
+// component imports
+import SinglePost from './SinglePost.vue'
 export default {
-  props:['posts'],
+  props: ['posts'],
   components: { SinglePost },
-  setup(props) {
-    onMounted(() => { console.log('component mounted')})
-    onUnmounted(() => { console.log('component unmounted')})
-    onUpdated(() => {console.log('component updated')})
+  setup() {
   }
 }
 </script>
 
 <style>
-
 </style>
